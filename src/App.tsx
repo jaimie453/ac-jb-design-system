30463158c28909401cfb01fca167a7d2ef218cfd
Jaimie Bowen <jaimie6453@gmail.com>
2023-02-03 19:12:48 -0500

    set up font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Button } from './components/button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import './app.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Button label="Button component" onClick={handleClick} />
 
       {/* This button won't inherit the styling from the Button component above, 
         since the component is styled using a .module.scss file*/}
       <button>Button tag</button>
-    </div>
+
+      <FontAwesomeIcon icon={faCoffee} />
+    </>
   );
 }
 
